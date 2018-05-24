35ea49f92cf98695ed917e6d96ca17534f7184c3
Oleh Cherednichenko <oleg.cherednichenko@wimdu.com>
2018-05-24 15:55:08 +0200

    QR Two-Factor Authentication

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def render_qr(user)
+    RQRCode::QRCode.new(
+      user.otp_provisioning_uri(
+        "#{Rails.application.secrets.issuer}:#{user.email}",
+        issuer: (issuer)
+        )
+      ).as_html
+  end
+
+  private
+
+  def issuer
+    'TwoFactorRailsProject'
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :registerable, :rememberable, :trackable, :validatable,
+         :two_factor_authenticatable,
+         :otp_secret_encryption_key => Rails.application.secrets.otp_key
+
+  after_create :generate_otp_secret!
+
+  private
+
+  def generate_otp_secret!
+    self.otp_secret = User.generate_otp_secret
+    self.save!
+  end
 end
